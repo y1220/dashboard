12ec579db832fcc93fa63080d63c3a5563e28fc1
y1220 <ga.2.y1220@gmail.com>
2024-12-06 23:31:25 +0100

    feat(PersonaGraph): adjust chart to be 3 bar chats

--- app/javascript/controllers/barchart_controller.js
+++ app/javascript/controllers/barchart_controller.js
@@ -14,26 +14,20 @@ export default class extends Controller {
     new Chart(this.canvasContext(), {
       type: "bar",
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Persona 1", "Persona 2", "Persona 3"],
         datasets: [
           {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
+            label: "# of people to be classified",
+            data: [12, 19, 3],
             backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
               "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
             ],
             borderColor: [
               "rgba(255, 99, 132, 1)",
               "rgba(54, 162, 235, 1)",
               "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
             ],
             borderWidth: 1,
           },
