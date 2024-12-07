e9d5f6d5cb7e1c6347aa7bd9ff6255b6986dd935
y1220 <ga.2.y1220@gmail.com>
2024-12-07 23:21:26 +0100

    chore(DynamicGraph): add api call to return numbers

--- app/controllers/api/personality_types_controller.rb
+++ app/controllers/api/personality_types_controller.rb
@@ -17,4 +17,24 @@ class Api::PersonalityTypesController < ApplicationController
 
     render json: @types
   end
+
+  def persona_statistics
+    # @types = PersonalityType.includes(:patients)
+    #                        .map do |type|
+    #   {
+    #     type: type.name,
+    #     count: type.patients.count,
+    #     avg_response_time: type.patients.joins(:communications)
+    #                           .average('communications.response_time'),
+    #     preferred_channel: type.patients.joins(:communications)
+    #                           .group('communications.channel')
+    #                           .order('count_id DESC')
+    #                           .count('id')
+    #                           .first&.first
+    #   }
+    # end
+    @types = {'numbers': [12, 19, 3]}
+
+    render json: @types
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :communications, only: [:index]
-    resources :personality_types, only: [:index]
+    resources :personality_types, only: [:index] do
+      collection do
+        get 'persona_statistics'
+      end
+    end
     resources :retention_rates, only: [:index]
     resources :response_times, only: [:index]
   end
