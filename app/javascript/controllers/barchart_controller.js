304de98ef51f3158008d74cc378bcc7f1cd4375f
y1220 <ga.2.y1220@gmail.com>
2024-12-07 23:20:05 +0100

    feat(DynamicGraph): add logic to show number dynamical fetched by api

--- app/javascript/controllers/barchart_controller.js
+++ app/javascript/controllers/barchart_controller.js
@@ -11,35 +11,40 @@ export default class extends Controller {
   }
 
   connect() {
-    new Chart(this.canvasContext(), {
-      type: "bar",
-      data: {
-        labels: ["Persona 1", "Persona 2", "Persona 3"],
-        datasets: [
-          {
-            label: "# of people to be classified",
-            data: [12, 19, 3],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
+    fetch("/api/personality_types/persona_statistics")
+      .then((response) => response.json())
+      .then((data) => {
+        new Chart(this.canvasContext(), {
+          type: "bar",
+          data: {
+            labels: ["Persona 1", "Persona 2", "Persona 3"],
+            datasets: [
+              {
+                label: "# of people to be classified",
+                data: data.numbers, // assuming the response has a 'numbers' array
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                  "rgba(255, 206, 86, 0.2)",
+                ],
+                borderColor: [
+                  "rgba(255, 99, 132, 1)",
+                  "rgba(54, 162, 235, 1)",
+                  "rgba(255, 206, 86, 1)",
+                ],
+                borderWidth: 1,
+              },
             ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-            ],
-            borderWidth: 1,
           },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
           },
-        },
-      },
-    });
+        });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }
 }
