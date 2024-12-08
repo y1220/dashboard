5fe1f076afd564126617d6db2efe403a568947b7
y1220 <ga.2.y1220@gmail.com>
2024-12-08 18:53:39 +0100

    fix(PersonaList): refine patient data to be dynamic

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,6 +1,7 @@
 class PatientController < ApplicationController
   def categorized_list
     @id = params[:id]
+    @patients = Patient.where(personality_type_id: @id)
     # Your logic here using the id parameter
   end
 end
