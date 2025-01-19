de1c0558fbc1fa214d3c2f5693c87ea31368b66f
y1220 <ga.2.y1220@gmail.com>
2025-01-20 00:39:59 +0100

    feat(Filter): add filter for patient list

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -2,6 +2,21 @@ class PatientController < ApplicationController
   def categorized_list
     @id = params[:id]
     @patients = Patient.where(personality_type_id: @id).page(params[:page]).per(10)
-    # Your logic here using the id parameter
+    # Apply filters if present
+    @patients = @patients.where("LOWER(name) LIKE ?", "%#{params[:name]}%") if params[:name].present?
+    @patients = @patients.where(gender: params[:gender]) if params[:gender].present?
+
+    # Age range filter
+    if params[:age_from].present? || params[:age_to].present?
+      if params[:age_from].present?
+        age_from_date = Date.today - params[:age_from].to_i.years
+        @patients = @patients.where("birthday <= ?", age_from_date)
+      end
+
+      if params[:age_to].present?
+        age_to_date = Date.today - params[:age_to].to_i.years
+        @patients = @patients.where("birthday >= ?", age_to_date)
+      end
+    end
   end
 end
